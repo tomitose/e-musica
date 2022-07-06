745cd0f519441f53e42dfd082e185c259c3c3089
tomitose <tomitosello@gmail.com>
2022-07-06 20:47:54 -0300

    Firebase mod

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1Rp0LyeM_8-3FSeaWs1WquphkmdlU-Bw",
+  authDomain: "ecommercemusicreact.firebaseapp.com",
+  projectId: "ecommercemusicreact",
+  storageBucket: "ecommercemusicreact.appspot.com",
+  messagingSenderId: "887871397293",
+  appId: "1:887871397293:web:ef89ab31672babf6eb1524"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
